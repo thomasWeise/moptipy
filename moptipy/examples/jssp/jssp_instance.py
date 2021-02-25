Fixed Lower Bound Computation for Makespan

moptipy/examples/jssp/jssp_instance.py
@@ -5,6 +5,7 @@ from moptipy.api import Component
 from moptipy.utils.logger import KeyValuesSection
 from moptipy.utils.nputils import int_range_to_dtype
 from typing import Final, List
+from moptipy.utils import nputils
 
 
 class JSSPInstance(Component):
@@ -86,12 +87,13 @@ class JSSPInstance(Component):
         sequence, i.e., 2*machine numbers.
         """
 
-        i64 = np.dtype(np.int64)
+# We now compute the lower bound for the makespan based on the algorithm
+# by Taillard
         usedmachines = np.zeros(machines, np.dtype(np.bool_))
-        jobtimes = np.zeros(jobs, i64)
-        machinetimes = np.zeros(machines, i64)
-        machine1 = np.zeros(machines, i64)
-        machine2 = np.zeros(machines, i64)
+        jobtimes = np.zeros(jobs, nputils.DEFAULT_INT)
+        machinetimes = np.zeros(machines, nputils.DEFAULT_INT)
+        machine1 = nputils.intmax(machines, nputils.DEFAULT_INT)
+        machine2 = nputils.intmax(machines, nputils.DEFAULT_INT)
 
         jobidx = 0
         for row in matrix:
@@ -116,13 +118,15 @@ class JSSPInstance(Component):
                 j += 2
 
             jobtimes[jobidx] = jobtime
-            j = 0
+            jobremaining = jobtime
+            j = len(row)
             for i in range(machines):
+                j -= 2
                 machine = row[j]
                 time = row[j + 1]
-                machine2[machine] = min(machine2[machine], jobtime)
-                jobtime -= time
-                j += 2
+                machine2[machine] = min(machine2[machine],
+                                        jobtime - jobremaining)
+                jobremaining -= time
 
             if not all(usedmachines):
                 ValueError("Some machines not used in a job in instance '"

moptipy/utils/nputils.py
@@ -1,4 +1,23 @@
 import numpy as np
+from typing import Final
+
+__NP_INTS: Final = \
+    tuple([(t[0], int(t[1].min), int(t[1].max))
+           for t in ((tt, np.iinfo(tt))
+                     for tt in (np.dtype(ttt)
+                                for ttt in [np.int8, np.uint8,
+                                            np.int16, np.uint16,
+                                            np.int32, np.uint32,
+                                            np.int64, np.uint64]))])
+
+DEFAULT_INT: Final = (__NP_INTS[len(__NP_INTS) - 2])[0]
+
+__NP_INT_MAX: Final = {}
+for i in __NP_INTS:
+    __NP_INT_MAX[i[0]] = i[2]
+__NP_INT_MIN: Final = {}
+for i in __NP_INTS:
+    __NP_INT_MIN[i[0]] = i[1]
 
 
 def int_range_to_dtype(min_value: int, max_value: int) -> np.dtype:
@@ -9,8 +28,8 @@ def int_range_to_dtype(min_value: int, max_value: int) -> np.dtype:
     The returned dtype will allow accommodating all values in the
     inclusive interval min_value..max_value.
 
-    :param int min_value: the min_valueimum value
-    :param int max_value: the max_valueimum value
+    :param int min_value: the minimum value
+    :param int max_value: the maximum value
     :return np.dtype: the numpy integer range
     """
 
@@ -25,31 +44,43 @@ def int_range_to_dtype(min_value: int, max_value: int) -> np.dtype:
                          + str(min_value) + " and max_value="
                          + str(max_value) + " was provided.")
 
-    if (min_value >= -128) and (max_value <= 127):
-        return np.dtype(np.int8)
-    if (min_value >= 0) and (max_value <= 255):
-        return np.dtype(np.uint8)
-    if (min_value >= -32768) and (max_value <= 32767):
-        return np.dtype(np.int16)
-    if (min_value >= 0) and (max_value <= 65535):
-        return np.dtype(np.uint16)
-    if (min_value >= -2147483648) and (max_value <= 2147483647):
-        return np.dtype(np.int32)
-    if (min_value >= 0) and (max_value <= 4294967295):
-        return np.dtype(np.uint32)
-    if (min_value >= -9223372036854775808) and \
-            (max_value <= 9223372036854775807):
-        return np.dtype(np.int64)
-    if max_value <= 18446744073709551615:
-        return np.dtype(np.uint64)
+    for t in __NP_INTS:
+        if (min_value >= t[1]) and (max_value <= t[2]):
+            return t[0]
 
+    ll = len(__NP_INTS) - 1
     if min_value >= 0:
         raise ValueError("max_value for unsigned integers must be "
-                         "less than 18446744073709551616, but is"
+                         "<=" + str((__NP_INTS[ll])[2]) + ", but is "
                          + str(max_value) + " for min_value "
                          + str(min_value) + ".")
 
+    ll = ll - 1
     raise ValueError("Signed integer range cannot exceed "
-                     "-9223372036854775808..9223372036854775807, "
-                     "but " + str(min_value) + ".." + str(max_value)
-                     + " specified.")
+                     + str((__NP_INTS[ll])[1]) + ".." + str((__NP_INTS[ll])[2])
+                     + ", but " + str(min_value) + ".." + str(max_value)
+                     + " was specified.")
+
+
+def intmax(shape, dtype: np.dtype = DEFAULT_INT) -> np.ndarray:
+    """
+    Create an integer array of the given length filled with the maximum value
+    :param shape: the requested shape
+    :param dtype: the data type (defaults to 64 bit integers)
+    :return: the new array
+    """
+    return np.full(shape=shape,
+                   fill_value=__NP_INT_MAX[dtype],
+                   dtype=dtype)
+
+
+def intmin(shape, dtype: np.dtype = DEFAULT_INT) -> np.ndarray:
+    """
+    Create an integer array of the given length filled with the minimum value
+    :param shape: the requested shape
+    :param dtype: the data type (defaults to 64 bit integers)
+    :return: the new array
+    """
+    return np.full(shape=shape,
+                   fill_value=__NP_INT_MIN[dtype],
+                   dtype=dtype)