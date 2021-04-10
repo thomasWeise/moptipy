5b7ccd07221b454fc4d29d22440d8d2008b6079a
Thomas Weise <tweise@hfuu.edu.cn>
2021-04-10 19:38:42 +0800

    plot_progress now handles both progress and stat runs

--- moptipy/evaluation/plot_defaults.py
+++ moptipy/evaluation/plot_defaults.py
@@ -1,11 +1,14 @@
 """Default styles for plots."""
-from typing import Union, Tuple, cast, List
+from typing import Union, Tuple, cast, List, Final, Dict
 
 import matplotlib.cm as mplcm  # type: ignore
 import matplotlib.colors as colors  # type: ignore
 import matplotlib.pyplot as plt  # type: ignore
 
 from moptipy.evaluation.base_classes import PerRunData, MultiRunData
+
+#: The internal color black.
+COLOR_BLACK: Final[Tuple[float, float, float]] = 0.0, 0.0, 0.0
 
 
 def key_func_inst(data: Union[PerRunData, MultiRunData]) -> str:
@@ -45,8 +48,8 @@ def default_name_func(key) -> str:
     return str(key)
 
 
-#: A set of predefined unique looking colors
-__FIXED_COLORS: Tuple[Tuple[Tuple[float, float, float], ...], ...] = \
+#: A set of predefined uniquely-looking colors.
+__FIXED_COLORS: Final[Tuple[Tuple[Tuple[float, float, float], ...], ...]] = \
     (((1.0, 0.0, 0.0), (0.0, 0.0, 1.0), (0.0, 1.0, 0.0),
       (0.6392156862745098, 0.14901960784313725, 0.14901960784313725),
       (1.0, 0.6470588235294118, 0.0), (0.0, 0.5333333333333333, 0.2),
@@ -126,3 +129,120 @@ def distinct_colors(n: int) -> Tuple[Tuple[float, float, float], ...]:
     if len(ss) != n:
         raise ValueError(f"Could not obtain {n} distinct colors.")
     return tuple(qq)
+
+
+#: An internal array of fixed line styles.
+__FIXED_LINESTYLES: \
+    Final[Tuple[Union[str, Tuple[float, Tuple[float, ...]]], ...]] = \
+    tuple(["solid",
+           "dotted",
+           "dashed",
+           "dashdot",
+           (0.0, (3.0, 5.0, 1.0, 5.0, 1.0, 5.0)),  # dashdotdotted
+           (0.0, (3.0, 1.0, 1.0, 1.0)),  # densely dashdotted
+           (0.0, (5.0, 1.0)),  # densely dashed
+           (0.0, (1.0, 1.0)),  # densely dotted
+           (0.0, (3.0, 1.0, 1.0, 1.0, 1.0, 1.0)),  # densely dashdotdotted
+           (0.0, (1.0, 10.0)),  # loosely dotted
+           (0.0, (5.0, 10.0)),  # loosely dashed
+           (0.0, (3.0, 10.0, 1.0, 10.0)),  # loosely dashdotted
+           (0.0, (3.0, 10.0, 1.0, 10.0, 1.0, 10.0))])  # loosely dashdotdotted
+
+
+def distinct_linestyles(n: int) -> \
+        Tuple[Union[str, Tuple[float, Tuple[float, ...]]], ...]:
+    """
+    Create a sequence of distinct line styles.
+
+    :param int n: the number of styles
+    :return: the styles
+    :rtype: Tuple[Union[str, Tuple[float, Tuple[float, float]]], ...]
+    """
+    if not isinstance(n, int):
+        raise TypeError(f"n must be int but is {type(n)}.")
+    if not (0 < n < 1000):
+        raise ValueError(f"Invalid n={n}.")
+    if n > len(__FIXED_LINESTYLES):
+        raise ValueError(f"{n} is too many for different strokes...")
+    if n == __FIXED_LINESTYLES:
+        return __FIXED_LINESTYLES
+    return tuple(__FIXED_LINESTYLES[0:n])
+
+
+def importance_to_linewidth(importance: int) -> float:
+    """
+    Transform an importance value to a line width.
+
+    Basically, an importance of `0` indicates a normal line in a normal
+    plot that does not need to be emphasized.
+    A positive importance means that the line should be emphasized.
+    A negative importance means that the line should be de-emphasized.
+
+    :param int importance: a value between -9 and 9
+    :return: the line width
+    :rtype: float
+    """
+    if not isinstance(importance, int):
+        raise TypeError(f"importance must be int but is {type(importance)}.")
+    if not (-10 < importance < 10):
+        raise ValueError(f"Invalid importance={importance}.")
+    if importance >= 0:
+        return 2.0 * (0.5 + importance)
+    if importance == -1:
+        return 2.0 / 3.0
+    if importance == -2:
+        return 0.5
+    return 0.7 ** (-importance)
+
+
+def importance_to_alpha(importance: int) -> float:
+    """
+    Transform an importance value to an alpha value.
+
+    Basically, an importance of `0` indicates a normal line in a normal
+    plot that does not need to be emphasized.
+    A positive importance means that the line should be emphasized.
+    A negative importance means that the line should be de-emphasized.
+
+    :param int importance: a value between -9 and 9
+    :return: the alpha
+    :rtype: float
+    """
+    if not isinstance(importance, int):
+        raise TypeError(f"importance must be int but is {type(importance)}.")
+    if not (-10 < importance < 10):
+        raise ValueError(f"Invalid importance={importance}.")
+    if importance >= 0:
+        return 1.0
+    if importance == -1:
+        return 2.0 / 3.0
+    if importance == -2:
+        return 0.5
+    return 1.0 / 3.0
+
+
+#: The internal default basic style
+__BASE_STYLE: Final[Dict[str, object]] = {
+    "alpha": 1.0,
+    "antialiased": True,
+    "color": COLOR_BLACK,
+    "dash_capstyle": "butt",
+    "dash_joinstyle": "round",
+    "linestyle": "solid",
+    "linewidth": 1.0,
+    "solid_capstyle": "round",
+    "solid_joinstyle": "round"
+}
+
+
+def create_style(**kwargs) -> Dict[str, object]:
+    """
+    Obtain the basic style for lines in diagrams.
+
+    :param kwargs: any additional overrides
+    :return: a dictionary with the style elements
+    :rtype: Dict[str, object]
+    """
+    res = dict(__BASE_STYLE)
+    res.update(kwargs)
+    return res
--- moptipy/evaluation/plot_progress_impl.py
+++ moptipy/evaluation/plot_progress_impl.py
@@ -1,42 +1,80 @@
 """Plot a set of progress objects into one figure."""
 from typing import List, Dict, Tuple, Final, Callable, Iterable, Union, \
-    Optional
+    Optional, Set, cast
 
+import matplotlib.lines as mlines  # type: ignore
+from matplotlib.artist import Artist  # type: ignore
 from matplotlib.figure import Figure, SubplotBase  # type: ignore
 
+import moptipy.evaluation.plot_defaults as pd
 from moptipy.evaluation.axis_ranger import AxisRanger
-from moptipy.evaluation.plot_defaults import key_func_inst, \
-    default_name_func, distinct_colors
 from moptipy.evaluation.progress import Progress
+from moptipy.evaluation.stat_run import StatRun
 
 
-def plot_progress(progresses: Iterable[Progress],
+def plot_progress(progresses: Iterable[Union[Progress, StatRun]],
                   figure: Union[SubplotBase, Figure],
                   x_axis: Union[AxisRanger, Callable] = AxisRanger.for_axis,
                   y_axis: Union[AxisRanger, Callable] = AxisRanger.for_axis,
-                  key_func: Callable = key_func_inst,
-                  name_func: Callable = default_name_func,
-                  distinct_colors_func: Callable = distinct_colors,
-                  legend: bool = True) -> None:
+                  legend: bool = True,
+                  key_func: Callable = pd.key_func_inst,
+                  name_func: Callable = pd.default_name_func,
+                  distinct_colors_func: Callable = pd.distinct_colors,
+                  distinct_linestyles_func: Callable = pd.distinct_linestyles,
+                  importance_to_linewidth_func: Callable =
+                  pd.importance_to_linewidth,
+                  importance_to_alpha_func: Callable =
+                  pd.importance_to_alpha) -> None:
     """
-    Plot a set of progress lines into one chart.
+    Plot a set of progress or statistical run lines into one chart.
 
     :param Iterable[moptipy.evaluation.Progress] progresses: the iterable
         of progresses
     :param Union[SubplotBase, Figure] figure: the figure to plot in
     :param Union[moptipy.evaluation.AxisRanger, Callable] x_axis: the x_axis
     :param Union[moptipy.evaluation.AxisRanger, Callable] y_axis: the y_axis
+    :param bool legend: should we plot the legend?
     :param Callable key_func: the function extracting the key from a progress
     :param Callable name_func: the function converting keys to names
     :param Callable distinct_colors_func: the function returning the palette
-    :param bool legend: should we plot the legend?
+    :param Callable distinct_linestyles_func: the function returning the line
+        styles
+    :param Callable importance_to_linewidth_func: the function converting
+        importance values to line widths
+    :param Callable importance_to_alpha_func: the function converting
+        importance values to alphas
     """
-    # find groups of runs to plot together in the same color/style
-    groups: Dict[object, List[Progress]] = dict()
+    # First, we try to find groups of data to plot together in the same
+    # color/style. We distinguish progress objects from statistical runs.
+    groups: Dict[object, Tuple[List[Progress], List[StatRun]]] = dict()
     x_dim: Optional[str] = None
     y_dim: Optional[str] = None
-
+    has_progress: bool = False
+    has_statrun: bool = False
+    stat_names_set: Set[str] = set()
     for prg in progresses:
+        # Compute the key identifying the right group for prg.
+        key = key_func(prg)
+
+        gp: Tuple[List[Progress], List[StatRun]]
+        if key in groups:
+            gp = groups[key]
+        else:
+            groups[key] = gp = list(), list()
+
+        # Check the type and decide to which list they should be added.
+        if isinstance(prg, Progress):
+            gp[0].append(prg)
+            has_progress = True
+        elif isinstance(prg, StatRun):
+            has_statrun = True
+            gp[1].append(prg)
+            stat_names_set.add(prg.stat_name)
+        else:
+            raise TypeError("Invalid progress object: "
+                            f"type {type(prg)} is not supported.")
+
+        # Validate that we have consistent time and objective units.
         if x_dim is None:
             x_dim = prg.time_unit
         elif x_dim != prg.time_unit:
@@ -49,23 +87,22 @@ def plot_progress(progresses: Iterable[Progress],
             raise ValueError(
                 f"F-units {y_dim} and {prg.f_name} do not fit!")
 
-        key = key_func(prg)
-        if key in groups:
-            groups[key].append(prg)
-        else:
-            groups[key] = [prg]
-
+    # Check if there was useful data to plot.
     n_groups: Final[int] = len(groups)
     if n_groups <= 0:
         raise ValueError("There must be at least one group.")
-    if (x_dim is None) or (y_dim is None):
+    if (x_dim is None) or (y_dim is None) or \
+            (not (has_progress or has_statrun)):
         raise ValueError("Illegal state?")
 
-    # create consistent orderings
+    # Create consistent orderings of the line groups.
     for runs in groups.values():
-        runs.sort()
+        for lst in runs:
+            cast(List, lst).sort()
 
-    # now name the data
+    # Now name the data consistently. For each key, one name
+    # will be computed. The data elements are then sorted by
+    # their names and the names can be used for the legend.
     key_list: List[object] = list(groups.keys())
     names_and_keys: Final[List[Tuple[str, object]]] = \
         [(name_func(key), key) for key in key_list]
@@ -76,11 +113,66 @@ def plot_progress(progresses: Iterable[Progress],
     names: Final[List[str]] = [key[0] for key in names_and_keys]
     del names_and_keys
 
+    # Compute the importance values for the line types and get the
+    # base style features line width and alpha.
+    progress_importance = 0
+    statrun_importance = 0
+    if has_statrun and has_progress:
+        progress_importance -= 1
+        statrun_importance += 1
+
+    progress_alpha = importance_to_alpha_func(progress_importance)
+    statrun_alpha = importance_to_alpha_func(statrun_importance)
+    progress_linewidth = importance_to_linewidth_func(progress_importance)
+    statrun_linewidth = importance_to_linewidth_func(statrun_importance)
+    colors = list(distinct_colors_func(n_groups))
+
+    plot_list: List[Dict] = list()
+    for groupidx, key in enumerate(keys):
+        for prgs in groups[key][0]:
+            plot_list.append(pd.create_style(x=prgs.time,
+                                             y=prgs.f,
+                                             color=colors[groupidx],
+                                             alpha=progress_alpha,
+                                             linewidth=progress_linewidth))
+
+    # Perform some mild, deterministic shuffling to obtain a fair printing
+    # order of lines: No line group should completely cover another one.
+    lll = len(plot_list)
+    if lll > 4:
+        center = lll // 2
+        for i in range(1, center, 2):
+            plot_list[i], plot_list[-i] = plot_list[-i], plot_list[i]
+        for start, end in [(center, lll - center), (lll - center, lll - 1)]:
+            for i in range(lll // 4):
+                plot_list[start + i], plot_list[end - i] = \
+                    plot_list[end - i], plot_list[start + i]
+
+    stat_names: Optional[List[str]] = None
+    stat_dashes: Optional[Tuple] = None
+    if has_statrun:
+        # Obtain the names of the statistics, if any.
+        stat_names = list(stat_names_set)
+        stat_names.sort()
+        stat_dashes = distinct_linestyles_func(len(stat_names))
+
+        for nameidx, stat_name in enumerate(stat_names):
+            for groupidx, key in enumerate(keys):
+                for strn in groups[key][1]:
+                    if strn.stat_name == stat_name:
+                        plot_list.append(pd.create_style(
+                            x=strn.stat[:, 0],
+                            y=strn.stat[:, 1],
+                            color=colors[groupidx],
+                            alpha=statrun_alpha,
+                            linewidth=statrun_linewidth,
+                            linestyle=stat_dashes[nameidx]))
+
+    del stat_names_set
+
     # set up the graphics area
     axes: Final = figure.add_axes([0.05, 0.05, 0.9, 0.9]) \
         if isinstance(figure, Figure) else figure.axes
-
-    colors = distinct_colors_func(n_groups)
 
     if callable(x_axis):
         x_axis = x_axis(x_dim)
@@ -92,29 +184,36 @@ def plot_progress(progresses: Iterable[Progress],
     if not isinstance(y_axis, AxisRanger):
         raise TypeError(f"y_axis must be AxisRanger, but is {type(y_axis)}.")
 
-    # plot the lines in a round robin fashio
-    found: bool = True
-    order: bool = True
-    while found:
-        found = False
-        steps = range(n_groups) if order else range(n_groups, 0, -1)
-        for index in steps:
-            key = keys[index]
-            runs = groups[key]
-            if runs:
-                found = True
-                run = runs.pop()
-                x_axis.register_array(run.time)
-                y_axis.register_array(run.f)
-                axes.step(run.time, run.f, color=colors[index],
-                          where="post")
+    # plot the lines in a round robin fashion
+    for line in plot_list:
+        axes.step(where="post", **line)
+        x_axis.register_array(line["x"])
+        y_axis.register_array(line["y"])
 
     x_axis.apply(axes, "x")
     y_axis.apply(axes, "y")
 
     if legend:
-        leg = axes.legend(loc="upper right",
-                          labels=names,
-                          labelcolor=colors)
-        for i, lgd in enumerate(leg.legendHandles):
-            lgd.set_color(colors[i])
+        handles: List[Artist] = [mlines.Line2D([], [],
+                                               color=colors[i],
+                                               label=name)
+                                 for i, name in enumerate(names)]
+
+        args: Dict[str, object] = dict()
+
+        if (len(stat_names) == 1) and (not has_progress):
+            args["title"] = stat_names[0]
+        else:
+            handles.extend([mlines.Line2D([], [],
+                                          color=pd.COLOR_BLACK,
+                                          linewidth=statrun_linewidth,
+                                          linestyle=stat_dashes[i],
+                                          label=name)
+                            for i, name in enumerate(stat_names)])
+            colors.extend([pd.COLOR_BLACK] * len(stat_names))
+
+        args["loc"] = "upper right"
+        args["handles"] = handles
+        args["labelcolor"] = colors
+
+        axes.legend(**args)
