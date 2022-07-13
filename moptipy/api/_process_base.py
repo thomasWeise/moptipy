7a559f4a83cf01ec03c55d72dd2083a4e7f6e450
Thomas Weise <tweise@hfuu.edu.cn>
2022-07-13 09:15:56 +0800

    restructuring of process base classes to prepare for MO optimization

--- moptipy/api/_process_base.py
+++ moptipy/api/_process_base.py
@@ -1,16 +1,111 @@
 """An internal module with the base class for implementing Processes."""
-from math import inf
+import os
+from io import StringIO
+from math import inf, isfinite
 from threading import Lock, Timer
 from time import time_ns
-from typing import Optional, Final, Union, Callable
+from traceback import print_tb
+from typing import Optional, Union, Final, Callable, Dict
 
+from numpy.random import Generator
+
+from moptipy.api.algorithm import Algorithm, check_algorithm
 from moptipy.api.logging import _ALL_SECTIONS, KEY_MAX_FES, \
-    KEY_MAX_TIME_MILLIS, SCOPE_PROCESS, KEY_GOAL_F
-from moptipy.api.process import Process, check_goal_f, check_max_fes, \
+    KEY_MAX_TIME_MILLIS, SCOPE_PROCESS, KEY_GOAL_F, KEY_EXCEPTION_TYPE, \
+    KEY_EXCEPTION_VALUE, KEY_EXCEPTION_STACK_TRACE, KEY_RAND_SEED, \
+    KEY_RAND_GENERATOR_TYPE, KEY_RAND_BIT_GENERATOR_TYPE, SCOPE_ALGORITHM, \
+    SCOPE_SOLUTION_SPACE, SCOPE_OBJECTIVE_FUNCTION, SECTION_FINAL_STATE, \
+    KEY_TOTAL_FES, KEY_TOTAL_TIME_MILLIS, KEY_BEST_F, \
+    KEY_LAST_IMPROVEMENT_FE, KEY_LAST_IMPROVEMENT_TIME_MILLIS, \
+    SECTION_SETUP, SECTION_RESULT_Y, SECTION_ERROR_IN_RUN, \
+    SECTION_ERROR_IN_CONTEXT, SECTION_ERROR_INVALID_Y, SECTION_ERROR_BEST_F, \
+    SECTION_ERROR_INVALID_X, SECTION_ERROR_TIMING, SECTION_ERROR_IN_LOG
+from moptipy.api.objective import Objective, check_objective
+from moptipy.api.process import Process, check_max_fes, \
     check_max_time_millis
-from moptipy.utils.logger import KeyValueLogSection
-from moptipy.utils.logger import SECTION_START, SECTION_END
-from moptipy.utils.types import type_error
+from moptipy.api.process import check_goal_f
+from moptipy.api.space import Space, check_space
+from moptipy.utils.logger import KeyValueLogSection, FileLogger, Logger, \
+    SECTION_START, SECTION_END
+from moptipy.utils.nputils import rand_generator, rand_seed_generate, \
+    rand_seed_check
+from moptipy.utils.path import Path
+from moptipy.utils.sys_info import log_sys_info
+from moptipy.utils.types import type_name_of, type_error
+
+
+def _error_1(logger: Logger, title: str, exception_type,
+             exception_value, traceback):
+    """
+    Create a text section with error information as from a contextmanager.
+
+    :param logger: the logger to write to
+    :param title: the title of the section with error information to be
+        created
+    :param exception_type: the exception type
+    :param exception_value: the exception value
+    :param traceback: the traceback
+    """
+    if exception_type or exception_value or traceback:
+        with logger.text(title=title) as ts:
+            if exception_type:
+                ts.write(KEY_EXCEPTION_TYPE)
+                ts.write(": ")
+                if isinstance(exception_type, str):
+                    if exception_type.startswith("<class '"):
+                        exception_type = exception_type[8:-2]
+                    ts.write(exception_type.strip())
+                else:
+                    ts.write(type_name_of(exception_type))
+                ts.write(os.linesep)
+            if exception_value:
+                ts.write(KEY_EXCEPTION_VALUE)
+                ts.write(": ")
+                ts.write(exception_value.strip())
+                ts.write(os.linesep)
+            if traceback:
+                ts.write(KEY_EXCEPTION_STACK_TRACE)
+                ts.write(":")
+                ts.write(os.linesep)
+                sio = StringIO()
+                print_tb(traceback, file=sio)
+                for line in sio.getvalue().split("\n"):
+                    ts.write(line.strip())
+                    ts.write(os.linesep)
+
+
+def _error_2(logger: Logger, title: str, exception: BaseException):
+    """
+    Log an exception.
+
+    :param logger: the logger to write to
+    :param title: the title of the section with error information to be
+        created
+    :param exception: the exception
+
+    >>> from moptipy.utils.logger import InMemoryLogger
+    >>> ime = InMemoryLogger()
+    >>> def k():
+    ...     1 / 0
+    >>> try:
+    ...     k()
+    ... except BaseException as be:
+    ...     _error_2(ime, "ERROR", be)
+    >>> print(ime.get_log()[0])
+    BEGIN_ERROR
+    >>> print(ime.get_log()[1])
+    exceptionType: ZeroDivisionError
+    >>> print(ime.get_log()[2])
+    exceptionValue: division by zero
+    >>> print(ime.get_log()[3])
+    exceptionStackTrace:
+    >>> print(ime.get_log()[-1])
+    END_ERROR
+    """
+    _error_1(logger, title, exception_type=exception,
+             exception_value=str(exception),
+             traceback=exception.__traceback__)
+
 
 # the function used to get the time
 _TIME_IN_NS: Final[Callable[[], int]] = time_ns
@@ -47,12 +142,22 @@ class _ProcessBase(Process):
     """The internal base class for implementing optimization processes."""
 
     def __init__(self,
+                 solution_space: Space,
+                 objective: Objective,
+                 algorithm: Algorithm,
+                 log_file: Optional[Path] = None,
+                 rand_seed: Optional[int] = None,
                  max_fes: Optional[int] = None,
                  max_time_millis: Optional[int] = None,
                  goal_f: Union[int, float, None] = None) -> None:
         """
-        Initialize information that every black-box process must have.
+        Perform the internal initialization. Do not call directly.
 
+        :param solution_space: the search- and solution space.
+        :param objective: the objective function
+        :param algorithm: the optimization algorithm
+        :param log_file: the optional log file
+        :param rand_seed: the optional random seed
         :param max_fes: the maximum permitted function evaluations
         :param max_time_millis: the maximum runtime in milliseconds
         :param goal_f: the goal objective value. if it is reached, the process
@@ -63,8 +168,6 @@ class _ProcessBase(Process):
         self._terminated: bool = False  # +book
         #: This becomes `True` when :meth:`should_terminate` returned `True`.
         self._knows_that_terminated: bool = False
-        #: The time when the process was started, in nanoseconds.
-        self._start_time_nanos: Final[int] = _TIME_IN_NS()
         #: The internal lock, needed to protect :meth:`terminate`.
         self.__lock: Final[Lock] = Lock()
         #: The maximum FEs.
@@ -86,6 +189,52 @@ class _ProcessBase(Process):
         self._last_improvement_time_nanos: int = -1
         #: The FE when the last improvement was made.
         self._last_improvement_fe: int = -1
+
+        #: The solution space, i.e., the data structure of possible solutions.
+        self._solution_space: Final[Space] = check_space(solution_space)
+        #: The objective function rating candidate solutions.
+        self.__objective: Final[Objective] = check_objective(objective)
+        #: the internal invoker for the objective function
+        self._f: Final[Callable] = self.__objective.evaluate
+        #: The algorithm to be applied.
+        self.__algorithm: Final[Algorithm] = check_algorithm(algorithm)
+        #: The random seed.
+        self.__rand_seed: Final[int] = rand_seed_generate() \
+            if rand_seed is None \
+            else rand_seed_check(rand_seed)
+        #: The random number generator.
+        self.__random: Final[Generator] = rand_generator(self.__rand_seed)
+        #: The current best solution.
+        self._current_best_y = solution_space.create()
+        #: The current best objective value
+        self._current_best_f: Union[int, float] = inf
+        #: The log file, or `None` is needed
+        if log_file is not None:
+            if not isinstance(log_file, Path):
+                raise type_error(log_file, "log_file", Path)
+        self.__log_file: Final[Optional[Path]] = log_file
+        #: the method for copying y
+        self._copy_y: Final[Callable] = solution_space.copy
+        #: set up the method forwards
+        self.lower_bound = self.__objective.lower_bound  # type: ignore
+        if self._end_f <= -inf:
+            self._end_f = check_goal_f(self.lower_bound())
+            self.lower_bound = lambda: self._end_f  # type: ignore
+        self.upper_bound = objective.upper_bound  # type: ignore
+        self.is_always_integer = objective.is_always_integer  # type: ignore
+        self.create = solution_space.create  # type: ignore
+        self.copy = solution_space.copy  # type: ignore
+        self.to_str = solution_space.to_str  # type: ignore
+        self.is_equal = solution_space.is_equal  # type: ignore
+        self.from_str = solution_space.from_str  # type: ignore
+        self.n_points = solution_space.n_points  # type: ignore
+        self.validate = solution_space.validate  # type: ignore
+        #: the internal section logger
+        self.__sections: Optional[Dict[str, str]] = \
+            None if log_file is None else {}
+
+        #: The time when the process was started, in nanoseconds.
+        self._start_time_nanos: Final[int] = _TIME_IN_NS()
         #: The maximum runtime in milliseconds.
         self._max_time_millis: Final[Optional[int]] = \
             check_max_time_millis(max_time_millis, True)
@@ -114,6 +263,9 @@ class _ProcessBase(Process):
         """
         if self.__timer is not None:
             self.__timer.start()
+
+    def get_random(self) -> Generator:
+        return self.__random
 
     def should_terminate(self) -> bool:
         if self._terminated:
@@ -149,6 +301,19 @@ class _ProcessBase(Process):
                              "last improvement time.")
         return _ns_to_ms(self._last_improvement_time_nanos
                          - self._start_time_nanos)
+
+    def has_best(self) -> bool:
+        return self._current_fes > 0
+
+    def get_best_f(self) -> Union[int, float]:
+        if self._current_fes > 0:
+            return self._current_best_f
+        raise ValueError('No current best available.')
+
+    def get_copy_of_best_x(self, x) -> None:
+        if self._current_fes > 0:
+            return self._copy_y(x, self._current_best_y)
+        raise ValueError('No current best available.')
 
     def terminate(self) -> None:
         with self.__lock:
@@ -188,6 +353,11 @@ class _ProcessBase(Process):
                              self._max_time_millis)
         if not (self._goal_f is None):
             logger.key_value(KEY_GOAL_F, self._goal_f)
+        logger.key_value(KEY_RAND_SEED, self.__rand_seed, True)
+        logger.key_value(KEY_RAND_GENERATOR_TYPE,
+                         type_name_of(self.__random))
+        logger.key_value(KEY_RAND_BIT_GENERATOR_TYPE,
+                         type_name_of(self.__random.bit_generator))
 
     def log_parameters_to(self, logger: KeyValueLogSection) -> None:
         """
@@ -199,14 +369,12 @@ class _ProcessBase(Process):
         """
         with logger.scope(SCOPE_PROCESS) as sc:
             self._log_own_parameters(sc)
-
-    def __str__(self) -> str:
-        """
-        Get the name of this process implementation.
-
-        :return: "baseProcess"
-        """
-        return "baseProcess"
+        with logger.scope(SCOPE_ALGORITHM) as sc:
+            self.__algorithm.log_parameters_to(sc)
+        with logger.scope(SCOPE_SOLUTION_SPACE) as sc:
+            self._solution_space.log_parameters_to(sc)
+        with logger.scope(SCOPE_OBJECTIVE_FUNCTION) as sc:
+            self.__objective.log_parameters_to(sc)
 
     def add_log_section(self, title: str, text: str) -> None:
         """
@@ -238,3 +406,158 @@ class _ProcessBase(Process):
             raise ValueError(
                 f"text of section '{t}' must not contain '{SECTION_START}' or"
                 f" '{SECTION_END}' but is '{text}'")
+        if self.__sections is not None:
+            if title in self.__sections:
+                raise ValueError(f"section '{title}' already logged.")
+            self.__sections[title] = text.strip()
+
+    def _write_log(self, logger: Logger) -> None:
+        """Write the information gathered during optimization into the log."""
+        with logger.key_values(SECTION_FINAL_STATE) as kv:
+            kv.key_value(KEY_TOTAL_FES, self._current_fes)
+            kv.key_value(KEY_TOTAL_TIME_MILLIS,
+                         _ns_to_ms(self._current_time_nanos
+                                   - self._start_time_nanos))
+            if self._current_fes > 0:
+                kv.key_value(KEY_BEST_F, self._current_best_f)
+                kv.key_value(KEY_LAST_IMPROVEMENT_FE,
+                             self._last_improvement_fe)
+                kv.key_value(KEY_LAST_IMPROVEMENT_TIME_MILLIS,
+                             _ns_to_ms(self._last_improvement_time_nanos
+                                       - self._start_time_nanos))
+
+        with logger.key_values(SECTION_SETUP) as kv:
+            self.log_parameters_to(kv)
+
+        log_sys_info(logger)
+
+        if self._current_fes > 0:
+            with logger.text(SECTION_RESULT_Y) as txt:
+                txt.write(self._solution_space.to_str(self._current_best_y))
+
+    def _validate_x(self) -> None:
+        """Validate x, if it exists."""
+
+    def _check_timing(self) -> None:
+        """
+        Check whether there has been any timing errors.
+
+        :raises ValueError: if there is any timing error
+        """
+        if self._current_time_nanos < self._start_time_nanos:
+            raise ValueError(
+                f"current_time_nanos={self._current_time_nanos} < "
+                f"start_time_nanos={self._start_time_nanos}")
+        if self._current_fes <= 0:
+            raise ValueError("no FE was performed")
+        if self._current_fes < self._last_improvement_fe:
+            raise ValueError(
+                f"current_fe={self._current_fes} < "
+                f"last_improvement_fe={self._last_improvement_fe}")
+        if self._current_time_nanos < self._last_improvement_time_nanos:
+            raise ValueError(
+                f"current_time_nanos={self._current_time_nanos} < "
+                "last_improvement_time_nanos="
+                f"{self._last_improvement_time_nanos}")
+
+    def _validate_best_f(self) -> None:
+        """
+        Validate the best encountered objective value.
+
+        :raises ValueError: if there is an error
+        """
+        ff: Final[Union[int, float]] = self._f(self._current_best_y)
+        if ff != self._current_best_f:
+            raise ValueError(  # noqa
+                "We re-computed the objective value of the best solution"
+                f"and got {ff}, but it has been registered as "
+                f"{self._current_best_f}!")  # noqa
+        if not isfinite(ff):
+            raise ValueError(  # noqa
+                f"Reproduced the objective value {ff} of the best "
+                "solution, but it is not finite?")  # noqa
+
+    def __exit__(self, exception_type, exception_value, traceback) -> None:
+        """Exit the process and write the log if necessary."""
+        # noinspection PyProtectedMember
+        super().__exit__(exception_type, exception_value, traceback)
+
+        # Update the total consumed time, but not include the error checks
+        # below.
+        self._current_time_nanos = _TIME_IN_NS()
+
+        y_error: Optional[BaseException] = None  # error in solution?
+        v_error: Optional[BaseException] = None  # error in objective value?
+        x_error: Optional[BaseException] = None  # error in search space?
+        t_error: Optional[BaseException] = None  # error in timing?
+        log_error: Optional[BaseException] = None  # error while logging?
+        try:
+            self._solution_space.validate(self._current_best_y)
+        except BaseException as be:
+            y_error = be
+        try:
+            self._validate_best_f()
+        except BaseException as be:
+            v_error = be
+        try:
+            self._validate_x()
+        except BaseException as be:
+            x_error = be
+        try:
+            self._check_timing()
+        except BaseException as be:
+            t_error = be
+
+        if self.__log_file is not None:
+            with FileLogger(self.__log_file) as logger:
+                try:
+                    self._write_log(logger)
+                except BaseException as be:
+                    log_error = be
+
+                if self._caught is not None:
+                    _error_2(logger, SECTION_ERROR_IN_RUN,
+                             self._caught)
+                if exception_type or exception_value or traceback:
+                    _error_1(logger, SECTION_ERROR_IN_CONTEXT,
+                             exception_type, exception_value, traceback)
+                if y_error:
+                    _error_2(logger, SECTION_ERROR_INVALID_Y, y_error)
+                if v_error:
+                    _error_2(logger, SECTION_ERROR_BEST_F, v_error)
+                if x_error:
+                    _error_2(logger, SECTION_ERROR_INVALID_X, x_error)
+                if t_error:
+                    _error_2(logger, SECTION_ERROR_TIMING, t_error)
+                if log_error:
+                    _error_2(logger, SECTION_ERROR_IN_LOG, log_error)
+
+                # flush all the additional log sections at the end
+                for t in sorted(self.__sections.keys()):
+                    with logger.text(t) as sec:
+                        sec.write(self.__sections[t])
+                del self.__sections
+
+        if not exception_type:
+            # if no error happened when closing the process, raise any error
+            # caught during validation.
+            if self._caught is not None:
+                raise self._caught  # pylint: disable=[E0702]
+            if y_error:
+                raise y_error
+            if v_error:
+                raise v_error
+            if x_error:
+                raise x_error
+            if t_error:
+                raise t_error
+            if log_error:
+                raise log_error
+
+    def __str__(self) -> str:
+        """
+        Get the name of this process implementation.
+
+        :return: "baseProcess"
+        """
+        return "baseProcess"
--- moptipy/api/mo_execution.py
+++ moptipy/api/mo_execution.py
@@ -1,12 +1,17 @@
 """The multi-objective algorithm execution API."""
 
-from typing import Optional
+from typing import Optional, Union
 
+from moptipy.api.algorithm import Algorithm
+from moptipy.api.encoding import Encoding
 from moptipy.api.execution import Execution
+from moptipy.api.mo_archive_pruner import MOArchivePruner, \
+    check_mo_archive_pruner
 from moptipy.api.mo_problem import MOProblem, check_mo_problem, \
     MOSOProblemBridge
 from moptipy.api.mo_process import MOProcess
 from moptipy.api.objective import Objective, check_objective
+from moptipy.api.space import Space
 from moptipy.utils.types import type_error
 
 
@@ -26,6 +31,8 @@ class MOExecution(Execution):
         self._archive_max_size: Optional[int] = None
         #: the archive size limit at which pruning should be performed
         self._archive_prune_limit: Optional[int] = None
+        #: the archive pruning strategy
+        self._archive_pruner: Optional[MOArchivePruner] = None
 
     def set_archive_max_size(self, size: int) -> 'MOExecution':
         """
@@ -55,6 +62,39 @@ class MOExecution(Execution):
         self._archive_max_size = size
         return self
 
+    def set_archive_pruning_limit(self, limit: int) -> 'MOExecution':
+        """
+        Set the size limit of the archive above which pruning is performed.
+
+        If the size of the archive grows above this limit, the archive will be
+        pruned down to the archive size limit.
+
+        :param limit: the archive pruning limit
+        :returns: this execution
+        """
+        if not isinstance(limit, int):
+            raise type_error(limit, "limit", int)
+        if limit <= 0:
+            raise ValueError(
+                f"archive pruning limit must be positive, but is {limit}.")
+        if self._archive_max_size is not None:
+            if limit < self._archive_max_size:
+                raise ValueError(
+                    f"archive pruning limit {limit} must be >= than archive "
+                    f"maximum size {self._archive_max_size}")
+        self._archive_prune_limit = limit
+        return self
+
+    def set_archive_pruner(self, pruner: MOArchivePruner) -> 'MOExecution':
+        """
+        Set the pruning strategy for downsizing the archive.
+
+        :param pruner: the archive pruner
+        :returns: this execution
+        """
+        self._archive_pruner = check_mo_archive_pruner(pruner)
+        return self
+
     def set_objective(self, objective: Objective) -> 'MOExecution':
         """
         Set the objective function in form of a multi-objective problem.
@@ -66,6 +106,144 @@ class MOExecution(Execution):
         if not isinstance(objective, MOProblem):
             objective = MOSOProblemBridge(objective)
         super().set_objective(check_mo_problem(objective))
+        return self
+
+    def set_algorithm(self, algorithm: Algorithm) -> 'MOExecution':
+        """
+        Set the algorithm to be used for this experiment.
+
+        :param algorithm: the algorithm
+        :returns: this execution
+        """
+        super().set_algorithm(algorithm)
+        return self
+
+    def set_solution_space(self, solution_space: Space) -> 'MOExecution':
+        """
+        Set the solution space to be used for this experiment.
+
+        This is the space managing the data structure holding the candidate
+        solutions.
+
+        :param solution_space: the solution space
+        :returns: this execution
+        """
+        super().set_solution_space(solution_space)
+        return self
+
+    def set_search_space(self, search_space: Optional[Space]) -> 'MOExecution':
+        """
+        Set the search space to be used for this experiment.
+
+        This is the space from which the algorithm samples points.
+
+        :param search_space: the search space, or `None` of none shall be
+            used, i.e., if search and solution space are the same
+        :returns: this execution
+        """
+        super().set_search_space(search_space)
+        return self
+
+    def set_encoding(self, encoding: Optional[Encoding]) -> 'MOExecution':
+        """
+        Set the encoding to be used for this experiment.
+
+        This is the function translating from the search space to the
+        solution space.
+
+        :param encoding: the encoding, or `None` of none shall be used
+        :returns: this execution
+        """
+        super().set_encoding(encoding)
+        return self
+
+    def set_rand_seed(self, rand_seed: Optional[int]) -> 'MOExecution':
+        """
+        Set the seed to be used for initializing the random number generator.
+
+        :param rand_seed: the random seed, or `None` if a seed should
+            automatically be chosen when the experiment is executed
+        """
+        super().set_rand_seed(rand_seed)
+        return self
+
+    def set_max_fes(self, max_fes: int,
+                    force_override: bool = False) -> 'MOExecution':
+        """
+        Set the maximum FEs.
+
+        This is the number of candidate solutions an optimization is allowed
+        to evaluate. If this method is called multiple times, then the
+        shortest limit is used unless `force_override` is `True`.
+
+        :param max_fes: the maximum FEs
+        :param force_override: the use the value given in `max_time_millis`
+            regardless of what was specified before
+        :returns: this execution
+        """
+        super().set_max_fes(max_fes, force_override)
+        return self
+
+    def set_max_time_millis(self, max_time_millis: int,
+                            force_override: bool = False) -> 'MOExecution':
+        """
+        Set the maximum time in milliseconds.
+
+        This is the maximum time that the process is allowed to run. If this
+        method is called multiple times, the shortest time is used unless
+        `force_override` is `True`.
+
+        :param max_time_millis: the maximum time in milliseconds
+        :param force_override: the use the value given in `max_time_millis`
+            regardless of what was specified before
+        :returns: this execution
+        """
+        super().set_max_time_millis(max_time_millis, force_override)
+        return self
+
+    def set_goal_f(self, goal_f: Union[int, float]) -> 'MOExecution':
+        """
+        Set the goal objective value after which the process can stop.
+
+        If this method is called multiple times, then the largest value is
+        retained.
+
+        :param goal_f: the goal objective value.
+        :returns: this execution
+        """
+        super().set_goal_f(goal_f)
+        return self
+
+    def set_log_file(self, log_file: Optional[str]) -> 'MOExecution':
+        """
+        Set the log file to write to.
+
+        This method can be called arbitrarily often.
+
+        :param log_file: the log file
+        """
+        super().set_log_file(log_file)
+        return self
+
+    def set_log_improvements(self, log_improvements: bool = True) \
+            -> 'MOExecution':
+        """
+        Set whether improvements should be logged.
+
+        :param log_improvements: if improvements should be logged?
+        :returns: this execution
+        """
+        super().set_log_improvements(log_improvements)
+        return self
+
+    def set_log_all_fes(self, log_all_fes: bool = True) -> 'MOExecution':
+        """
+        Set whether all FEs should be logged.
+
+        :param log_all_fes: if all FEs should be logged?
+        :returns: this execution
+        """
+        super().set_log_all_fes(log_all_fes)
         return self
 
     def execute(self) -> MOProcess:
