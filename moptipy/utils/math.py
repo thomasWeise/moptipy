ea690c3d795255568e0827939dfea4f242e37564
Thomas Weise <tweise@hfuu.edu.cn>
2023-02-19 18:19:06 +0800

    more tests and improvements of math routines

--- moptipy/utils/math.py
+++ moptipy/utils/math.py
@@ -1,7 +1,7 @@
 """Simple routines for handling numbers and numerical stuff."""
 
 import contextlib
-from math import exp, gcd, inf, isfinite, isqrt, log, log2, nextafter
+from math import exp, inf, isfinite, isqrt, log, log2, nextafter
 from typing import Final
 
 from moptipy.utils.types import type_error
@@ -18,6 +18,35 @@ def __try_int(val: float) -> int | float:
 
     :param val: the flot
     :returns: the float or int
+
+    >>> from math import inf, nan, nextafter
+    >>> type(__try_int(0.0))
+    <class 'int'>
+    >>> type(__try_int(0.5))
+    <class 'float'>
+    >>> type(__try_int(inf))
+    <class 'float'>
+    >>> type(__try_int(-inf))
+    <class 'float'>
+    >>> type(__try_int(nan))
+    <class 'float'>
+    >>> 1 << 53
+    9007199254740992
+    >>> type(__try_int(9007199254740992.0))
+    <class 'int'>
+    >>> __try_int(9007199254740992.0)
+    9007199254740992
+    >>> too_big = nextafter(9007199254740992.0, inf)
+    >>> print(too_big)
+    9007199254740994.0
+    >>> type(__try_int(too_big))
+    <class 'float'>
+    >>> type(__try_int(-9007199254740992.0))
+    <class 'int'>
+    >>> __try_int(-9007199254740992.0)
+    -9007199254740992
+    >>> type(__try_int(-too_big))
+    <class 'float'>
     """
     if _DBL_INT_LIMIT_N <= val <= DBL_INT_LIMIT_P:
         a = int(val)
@@ -36,14 +65,58 @@ def try_int(val: int | float) -> int | float:
     `-9007199254740992...9007199254740992`, i.e., the range where `+1` and
     `-1` work without loss of precision.
 
-    :param val: the input value
+    :param val: the input value, which must either be `int` or `float`
     :return: an `int` if `val` can be represented as `int` without loss of
         precision, `val` otherwise
+    :raises TypeError: if `val` is neither an instance of `int` nor of `float`
+    :raises ValueError: if `val` is a `float`, but not finite
 
     >>> print(type(try_int(10.5)))
     <class 'float'>
     >>> print(type(try_int(10)))
     <class 'int'>
+
+    >>> from math import inf, nan, nextafter
+    >>> type(try_int(0.0))
+    <class 'int'>
+    >>> type(try_int(0.5))
+    <class 'float'>
+    >>> try:
+    ...     try_int(inf)
+    ... except ValueError as ve:
+    ...     print(ve)
+    Value must be finite, but is inf.
+    >>> try:
+    ...     try_int(-inf)
+    ... except ValueError as ve:
+    ...     print(ve)
+    Value must be finite, but is -inf.
+    >>> try:
+    ...     try_int(nan)
+    ... except ValueError as ve:
+    ...     print(ve)
+    Value must be finite, but is nan.
+    >>> try:
+    ...     try_int("blab")  # noqa  # type: off
+    ... except TypeError as te:
+    ...     print(te)
+    val should be an instance of any in {float, int} but is str, namely \
+'blab'.
+    >>> type(try_int(9007199254740992.0))
+    <class 'int'>
+    >>> try_int(9007199254740992.0)
+    9007199254740992
+    >>> too_big = nextafter(9007199254740992.0, inf)
+    >>> print(too_big)
+    9007199254740994.0
+    >>> type(try_int(too_big))
+    <class 'float'>
+    >>> type(try_int(-9007199254740992.0))
+    <class 'int'>
+    >>> try_int(-9007199254740992.0)
+    -9007199254740992
+    >>> type(try_int(-too_big))
+    <class 'float'>
     """
     if isinstance(val, int):
         return val
@@ -66,55 +139,115 @@ def try_int_div(a: int, b: int) -> int | float:
     to avoid this here as much as possible. First, we check if `a` is
     divisible by `b` without any fractional part. If this is true, then
     we can do a pure integer division without loss of any precision.
-    Otherwise, we would do a floating point division. This will lead
-    the values be converted to floating point numbers, which can incur
-    a loss of precision. We therefore first compute the gcd of `a` and `b`
-    and integer-divide both numbers by this value. If the gcd is greater than
-    one, then this will make the numbers a bit "smaller", which would reduce
-    the loss of precision during the conversion. We then do the final floating
-    point division. As a very last step, we check if that result could be
-    converted back to an integer, which is very unlikely. This could only
-    happen if the division itself incured a loss of precision ... but we give
-    it a try anyway.
+
+    Otherwise, we would do a floating point division. This will lead the
+    values be converted to floating point numbers, which can incur
+    a loss of precision. In the past, we tried to divide both numbers by
+    their `gcd` to make them smaller in order to avoid a loss of precision.
+    But it seems that Python is already doing something like this internally
+    when performing the `a / b` division anyway, so we don't do this anymore.
+    However, it is still attempted to convert the result back to an integer.
 
     :param a: the first integer
     :param b: the second integer
-    :return: a/b
+    :return: a/b, either as `int` or as `float` but always a finite value
+    :raises OverflowError: if the division must be performed using floating
+        point arithmetic, but the result would be too large to fit into a
+        `float`
+    :raises ZeroDivisionError: if `b==0`
 
+    >>> print(try_int_div(10, 2))
+    5
+    >>> print(try_int_div(10, -2))
+    -5
+    >>> print(try_int_div(-10, 2))
+    -5
+    >>> print(try_int_div(-10, -2))
+    5
     >>> print(type(try_int_div(10, 2)))
     <class 'int'>
+    >>> print(try_int_div(10, 3))
+    3.3333333333333335
+    >>> print(try_int_div(-10, 3))
+    -3.3333333333333335
+    >>> print(try_int_div(10, -3))
+    -3.3333333333333335
+    >>> print(try_int_div(-10, -3))
+    3.3333333333333335
     >>> print(type(try_int_div(10, 3)))
     <class 'float'>
+    >>> print(try_int_div(9007199254740992, 1))
+    9007199254740992
+    >>> print(try_int_div(2109792310235001520128, 234234))
+    9007199254740992
+    >>> print(try_int_div(2109792310235001520128, 234235))
+    9007160801054503
+    >>> print(try_int_div(2109792310235001520128, 234233))
+    9007237708755818.0
+    >>> large = 123456789012345678901234567890123456789012345678901234567\
+89012345678901234567890123456789012345678901234567890123456789012345678901234\
+56789012345678901234567890123456789012345678901234567890123456789012345678901\
+23456789012345678901234567890123456789012345678901234567890123456789012345678\
+90123456789012345678901234567890123456789012345678901234567890123456789012345\
+67890123456789012345678901234567890123456789012345678901234567890123456789012\
+3456789012345678901234567890123456789012345678901234567890123456789012345678\
+90123456789012345678901234567890123456789012345678901234567890123456789012345\
+678901234567890123456789012345678901234567890
+    >>> try:
+    ...     large / 1
+    ... except OverflowError as oe:
+    ...     print(oe)
+    integer division result too large for a float
+    >>> try_int_div(large, 1)
+    123456789012345678901234567890123456789012345678901234567\
+89012345678901234567890123456789012345678901234567890123456789012345678901234\
+56789012345678901234567890123456789012345678901234567890123456789012345678901\
+23456789012345678901234567890123456789012345678901234567890123456789012345678\
+90123456789012345678901234567890123456789012345678901234567890123456789012345\
+67890123456789012345678901234567890123456789012345678901234567890123456789012\
+3456789012345678901234567890123456789012345678901234567890123456789012345678\
+90123456789012345678901234567890123456789012345678901234567890123456789012345\
+678901234567890123456789012345678901234567890
+    >>> try_int_div(large * 7, 1 * 7)
+    123456789012345678901234567890123456789012345678901234567\
+89012345678901234567890123456789012345678901234567890123456789012345678901234\
+56789012345678901234567890123456789012345678901234567890123456789012345678901\
+23456789012345678901234567890123456789012345678901234567890123456789012345678\
+90123456789012345678901234567890123456789012345678901234567890123456789012345\
+67890123456789012345678901234567890123456789012345678901234567890123456789012\
+3456789012345678901234567890123456789012345678901234567890123456789012345678\
+90123456789012345678901234567890123456789012345678901234567890123456789012345\
+678901234567890123456789012345678901234567890
+    >>> try:
+    ...     try_int_div(large, 7)
+    ... except OverflowError as oe:
+    ...     print(oe)
+    integer division result too large for a float
+    >>> try:
+    ...     try_int_div(0, 0)
+    ... except ZeroDivisionError as zde:
+    ...     print(zde)
+    integer division or modulo by zero
+    >>> try:
+    ...     try_int_div(1, 0)
+    ... except ZeroDivisionError as zde:
+    ...     print(zde)
+    integer division or modulo by zero
+    >>> try:
+    ...     try_int_div(-1, 0)
+    ... except ZeroDivisionError as zde:
+    ...     print(zde)
+    integer division or modulo by zero
     """
-    if b == 0:
-        return inf
-
     # First try pure integer division, in case `a` is divisible by `b`.
     int_div_test: Final[int] = a // b
     if (int_div_test * b) == a:
         return int_div_test
-
-    # No, pure division does not work.
-    # So let us next divide both `a` and `b` by their gcd.
-    gc: Final[int] = gcd(a, b)
-    a //= gc
-    if b == gc:
-        return a
-    b //= gc
-
-    # Then we can convert them to floating point numbers with less loss
-    # of precision.
-    val: Final[float] = a / b
-    if not isfinite(val):
-        raise ValueError(f"Value must be finite, but is {a}/{b}={val}.")
-    # Finally, we attempt to convert the result to an integer again.
-    # This can only work if there was some loss of precision during the
-    # division.
-    return __try_int(val)
+    # If that does not work, use the floating point division.
+    return __try_int(a / b)
 
 
-def try_float_div(a: int | float, b: int | float) \
-        -> int | float:
+def try_float_div(a: int | float, b: int | float) -> int | float:
     """
     Try to divide two numbers at best precision.
 
@@ -126,7 +259,33 @@ def try_float_div(a: int | float, b: int | float) \
 
     :param a: the first number
     :param b: the second number
-    :return: a/b
+    :return: `a/b`, but always finite
+
+    :raises ValueError: if either one of the arguments or the final result
+        would not be finite
+
+    >>> try_float_div(1e180, 1e60)
+    1.0000000000000001e+120
+    >>> try_float_div(1e60, 1e-60)
+    1e+120
+    >>> try_float_div(1e14, 1e-1)
+    1000000000000000
+    >>> try_float_div(1e14, -1e-1)
+    -1000000000000000
+    >>> try_float_div(-1e14, 1e-1)
+    -1000000000000000
+    >>> try_float_div(-1e14, -1e-1)
+    1000000000000000
+    >>> try_float_div(1e15, 1e-1)
+    1e+16
+    >>> try_float_div(1e15, -1e-1)
+    -1e+16
+    >>> try_float_div(-1e15, 1e-1)
+    -1e+16
+    >>> try_float_div(-1e15, -1e-1)
+    1e+16
+    >>> try_float_div(1e15, 1e-15)
+    9.999999999999999e+29
 
     >>> print(type(try_float_div(10, 2)))
     <class 'int'>
@@ -134,14 +293,69 @@ def try_float_div(a: int | float, b: int | float) \
     <class 'float'>
     >>> print(type(try_float_div(10, 0.5)))
     <class 'int'>
+
+    >>> from math import inf, nan
+    >>> try:
+    ...     try_float_div(1.0, 0)
+    ... except ZeroDivisionError as zde:
+    ...     print(zde)
+    integer division or modulo by zero
+
+    >>> try:
+    ...     try_float_div(1.0, -0.0)
+    ... except ZeroDivisionError as zde:
+    ...     print(zde)
+    integer division or modulo by zero
+
+    >>> try:
+    ...     try_float_div(inf, 0)
+    ... except ValueError as ve:
+    ...     print(ve)
+    Value must be finite, but is inf.
+
+    >>> try:
+    ...     try_float_div(-inf, 0)
+    ... except ValueError as ve:
+    ...     print(ve)
+    Value must be finite, but is -inf.
+
+    >>> try:
+    ...     try_float_div(nan, 0)
+    ... except ValueError as ve:
+    ...     print(ve)
+    Value must be finite, but is nan.
+
+    >>> try:
+    ...     try_float_div(1, inf)
+    ... except ValueError as ve:
+    ...     print(ve)
+    Value must be finite, but is inf.
+
+    >>> try:
+    ...     try_float_div(1, -inf)
+    ... except ValueError as ve:
+    ...     print(ve)
+    Value must be finite, but is -inf.
+
+    >>> try:
+    ...     try_float_div(1, nan)
+    ... except ValueError as ve:
+    ...     print(ve)
+    Value must be finite, but is nan.
+
+    >>> try:
+    ...     try_float_div(1e300, 1e-60)
+    ... except ValueError as ve:
+    ...     print(ve)
+    Result must be finite, but is 1e+300/1e-60=inf.
     """
-    ia = try_int(a)
-    ib = try_int(b)
+    ia: Final[int | float] = try_int(a)
+    ib: Final[int | float] = try_int(b)
     if isinstance(ia, int) and isinstance(ib, int):
         return try_int_div(ia, ib)
     val: Final[float] = ia / ib
     if not isfinite(val):
-        raise ValueError(f"Value must be finite, but is {a}/{b}={val}.")
+        raise ValueError(f"Result must be finite, but is {a}/{b}={val}.")
     return __try_int(val)
 
 
